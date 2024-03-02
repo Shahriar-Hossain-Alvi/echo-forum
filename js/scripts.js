48380de24bad586411cfafe0314d839e7a43a7f3
Md. Shahriar Hossain Alvi <shahriarhossainalvi@gmail.com>
2024-03-03 00:49:36 +0600

    navbar added

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,13 @@
-console.log('connected!');
+//spinner
+window.addEventListener('load', () => {
+    const spinner = document.getElementById('spinner');
+    spinner.style.display = 'block';
+
+    setTimeout(function () {
+        spinner.style.display = 'none';
+    }, 2000);
+
+});
+
+
+
