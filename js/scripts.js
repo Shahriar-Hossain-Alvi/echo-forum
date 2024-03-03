94bea50150f431a57752e7b7e318dce4a97bf6aa
Md. Shahriar Hossain Alvi <shahriarhossainalvi@gmail.com>
2024-03-03 12:08:51 +0600

    added allpost api loader

--- js/scripts.js
+++ js/scripts.js
@@ -9,5 +9,14 @@ window.addEventListener('load', () => {
 
 });
 
+//fetch data
+const loadAllPostData = async () => {
+    const res = await fetch('https://openapi.programming-hero.com/api/retro-forum/posts');
+    const data = await res.json();
+    console.log(data);
+}
+
+loadAllPostData();
+
 
 
